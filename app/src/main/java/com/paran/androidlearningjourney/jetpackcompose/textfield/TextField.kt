54c7cff6faa4f8808e8600ceeed70f73987492bb
Paran <saahnafparan@gmail.com>
2026-09-10 23:58:34 +0600

    Added code for TextField

---
+++ app/src/main/java/com/paran/androidlearningjourney/jetpackcompose/textfield/TextField.kt
@@ -0,0 +1,66 @@
+package com.paran.androidlearningjourney.jetpackcompose.textfield
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+ fun TextField() {
+
+     var userName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+
+
+    ) {
+
+        TextField(
+            value = userName,
+            onValueChange = { userName = it })
+
+
+        TextField(
+            value = email,
+            onValueChange = { email = it })
+
+
+
+
+        Text(
+            text = "🚀 Ultimate Android Learning Journey",
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+
+
+
+
+
+
+
+
+}
