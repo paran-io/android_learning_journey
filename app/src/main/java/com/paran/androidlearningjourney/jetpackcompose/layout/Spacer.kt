5489c5158a98342bb830afac9430e535ac801f63
Paran <saahnafparan@gmail.com>
2026-09-26 13:05:09 +0600

    Update Jetpack Compose Spacer tutorial code

--- app/src/main/java/com/paran/androidlearningjourney/jetpackcompose/layout/Spacer.kt
+++ app/src/main/java/com/paran/androidlearningjourney/jetpackcompose/layout/Spacer.kt
@@ -2,11 +2,17 @@ package com.paran.androidlearningjourney.jetpackcompose.layout
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MenuOpen
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +23,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -60,6 +67,42 @@ fun SpacerExample() {
             modifier = Modifier.fillMaxWidth()
 
         )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+
+        Row(modifier = Modifier.fillMaxWidth()
+            .height(60.dp),
+            verticalAlignment = Alignment.CenterVertically
+
+
+        ) {
+
+            Text(text = "KMP(Kotlin Multiplatform)",
+                fontSize = 19.sp,
+                textAlign = TextAlign.Start,
+
+
+            )
+
+            Spacer(Modifier.width(20.dp))
+
+
+            Text(text = "KMP",
+                fontSize = 20.sp
+
+            )
+
+
+
+        }
+
+
+
+
+
+
+
 
     }
 
